cd670ce02deef0b0655bf64041e423aa048a3cd9
Thomas ASTLEITNER <Thomas.ASTLEITNER@bachmann.info>
2022-05-05 15:09:55 +0200

    Node status, sending output, configurable timeout unit.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 Secure your smart home with this monitoring node. It monitors signals and triggers the output on unexpected events.
+
+# Input
+## Arming and disarming
+You must send a message with a defined topic and payload. The **topic** can be freely configured in the node settings. If unconfigured, "activate" is expected. The **payload** must be boolean `true` to arm or `false` to disarm.
+## Signals to monitor
+Basically, you can connect everything on the input. As soon as the node receives any message (no matter if and what topic or message it contains, really every message counts), the timer starts and warning will be sent in the output object. If timeout is set to zero, error will be immediately set.
+
+# Configuration
+## Name
+If unset, the node shows "Monitor". If you give any value here, the node will be shown with this string.
+## Arming topic
+A defined message arms or disarms the node. The message must contain a boolean `true` or `false` to arm or disarm and must contain a defined topic. If this field is left blank, the topic must be "activate".
+## Warning time
+When armed, the node watches out for any message on the input. If a message appears, a warning will be sent inside the output message but the alarm is still false, until the warning time has been reached. That's the time when you open a window but have not disarmed the node until the siren shouts.
+## Allow msg.timeout to verwrite
+Together with the TBC
+## Debug
+
+# Node status
+
+# Warning and error codes
+Warnings and errors will be sent both to the integrated Node-RED debugger and the terminal, so it may be a good idea to have focus to at least one of them. If a warning or an error happens, it will be sent to there with a leading identifier. This starts either with "W" for warnings or "E" for errors. Example: "E007: Invalid James Bond detected, replace!" for an error. Here's the list of codes supported by this individual node:
+| Identifier | Description |
+| - | - |
+| E010 | Warning time unit invalid. In your node configuration, you have to set a timeout value (or use "0" to disable) together with a unit (Seconds, Minutes, Hours). As this unit has to be set from a dropdown, there should not be a way the node receives anything else than one of these. Anyways, the unit will be checked and if, for some weird reason, it is not one of those selectable, you will see this error. Get in touch with the developer to get that fixed.
--- nodes/monitor.js
+++ nodes/monitor.js
@@ -2,10 +2,10 @@
 
 module.exports = function(RED) {
 
-	let active = false
+	let armed = false
 	let warning = false
 	let alert = false
-	let warningTimeoutHandle
+	let warningTime, warningTimeoutHandle
 
 	function monitor(node) {
 		RED.nodes.createNode(this, node)
@@ -21,7 +21,7 @@ module.exports = function(RED) {
 		 */
 		let context = nodeContext.get("context")
 		if (!context) {
-			that.warn("No previous sensor states to restore. See https://nodered.org/docs/user-guide/context#saving-context-data-to-the-file-system how to persistently save states.")
+			// that.warn("No previous sensor states to restore. See https://nodered.org/docs/user-guide/context#saving-context-data-to-the-file-system how to persistently save states.")
 			context = {
 				items: {}
 			}
@@ -29,53 +29,40 @@ module.exports = function(RED) {
 
 		
 		// FUNCTIONS ====>
-		
+
 		function setNodeStateFunc() {
-			// TODO working with state
-			// TODO different appearance if num = 0
-			const numItems = Object.keys(context.items).length
-			let fill, shape, sinpluStr, text
+			let fill, shape, text
 
-			console.log("active: " & active & ", warning: " & warning & ", alert: " & alert)
-
-			if (numItems === 1) {
-				sinpluStr = "item"
-			} else {
-				sinpluStr = "items"
-			}
-
-			if (!active) {
+			if (!armed) {
 				fill = "grey"
 				shape = "ring"
-				// text = numItems + " " + sinpluStr + " in storage"		// TODO preperation for rbe
-				text = "Inactive"
-			} else if (active) {
+				text = "Disarmed"
+			} else {
 				fill = "green"
 				shape = "dot"
-				// text = "Monitoring " + numItems + " " + sinpluStr		// TODO preperation for rbe
-				text = "Active"
+				text = "Armed"
+			}
+			
+			if (alert) {
+				fill = "red"
+				shape = "dot"
+				text = "Alert"
 			} else if (warning) {
 				fill = "yellow"
 				shape = "dot"
 				text = "Warning"
-			} else if (alert) {
-				fill = "red"
-				shape = "dot"
-				text = "Alert"
-			} else {
-				fill = "blue"
-				shape = "ring"
-				text = "Unknown state"
 			}
 			
 			that.status({fill: fill, shape: shape, text: text})
+			that.send({topic: "status", payload: {"armed": armed, "warning": warning, "alert": alert}})
+
 		}
 
 
 		function alertFunc() {
 			warning = false
 			alert = true
-			that.error("ALERT")
+			setNodeStateFunc()
 		}
 
 		// <==== FUNCTIONS
@@ -85,6 +72,14 @@ module.exports = function(RED) {
 
 		setNodeStateFunc()
 
+		let warningTimeFactor
+		if (node.warningTimeUnit === "s") {warningTimeFactor = 1000}
+		else if (node.warningTimeUnit === "m") {warningTimeFactor = 60000}
+		else if (node.warningTimeUnit === "h") {warningTimeFactor = 3600000}
+		else {that.error("E010: Warning time unit invalid")}
+		
+		warningTime = Number(node.warningTime) * warningTimeFactor
+		
 		// TODO verify node.monitorTopic
 
 		// <==== FIRST RUN ACTIONS
@@ -99,62 +94,27 @@ module.exports = function(RED) {
 			if (msg.topic === "activate") {		// TODO make configurable
 				clearTimeout(warningTimeoutHandle)
 				if (msg.payload === true) {
-					active = true		// TODO ist active im context am besten untergebracht??
+					armed = true		// TODO ist active im context am besten untergebracht??
 					warning = false
 					alert = false
 					setNodeStateFunc()
 				} else if (msg.payload === false) {
-					active = false
+					armed = false
 					warning = false
 					alert = false
 					setNodeStateFunc()
 				} else {
-					// TODO send message: invalid payload (not bool) on activate
+					// TODO send message: invalid payload (not bool)
 				}
-			}
-			
-
-			// TODO The following is a preperation for rbe filtering
-			// // ITEM MESSAGE
-
-			// else {
-			// 	// TODO validate message (topic and payload)
-			// 	// TODO give possibility to reset items
-				
-			// 	let task = null
-
-			// 	if (active) {
-			// 		if (context.items[msg.topic] != msg.payload) {
-			// 			task = "alarm"
-			// 		}
-			// 	}
-
-			// 	context.items[msg.topic] = msg.payload		// Add new item to store
-
-			// 	msg = {
-			// 		items: context.items, // TODO maybe send this only if debug is active
-			// 		task: task
-			// 	}
-	
-			// 	that.send(msg)
-			// 	setNodeState()
-
-			// }
-
-
-			else {
+			} else if (armed) {
 				if (node.warningTime > 0) {
-					warningTimeoutHandle = setTimeout(alertFunc, node.warningTime * 1000)
+					warningTimeoutHandle = setTimeout(alertFunc, warningTime)
 					warning = true
 					setNodeStateFunc()
-					that.error("WARNING")
 				} else {
 					alertFunc()
-					setNodeStateFunc()
 				}
 			}
-
-
 
 
 			if (err) {
